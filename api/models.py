4a647d06750babbf94e9557f91fd87ba5c071608
stephenOwino <36981339s@gmail.com>
2025-09-16 02:09:03 +0300

    add Customer model with User relation, phone number, and address

--- api/models.py
+++ api/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Customer(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    phone_number = models.CharField(max_length=20)
+    address = models.TextField()
+
+    def __str__(self):
+        return self.user.username
+    
+    
+ 
