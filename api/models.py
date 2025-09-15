354df60b60610ce5f1949c13bc4a08c6fea3e930
stephenOwino <36981339s@gmail.com>
2025-09-16 02:16:42 +0300

    add Product model with category relation

--- api/models.py
+++ api/models.py
@@ -21,3 +21,14 @@ class Category(models.Model):
 
     def __str__(self):
         return self.name    
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='products')
+    stock = models.PositiveIntegerField()
+
+    def __str__(self):
+        return self.name
