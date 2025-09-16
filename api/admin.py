052eeec023653897fd96b3e1df9a6b0408340584
stephenOwino <36981339s@gmail.com>
2025-09-16 11:45:50 +0300

    Refactor admin.py: Add ModelAdmin classes and inline for better admin management

--- api/admin.py
+++ api/admin.py
@@ -3,8 +3,30 @@ from django.contrib import admin
 from .models import Customer, Category, Product, Order, OrderItem
 
 # Register your models here.
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'parent')
+    search_fields = ('name',)
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'category', 'price', 'stock')
+    list_filter = ('category',)
+    search_fields = ('name', 'description')
+
+class OrderItemInline(admin.TabularInline):
+    model = OrderItem
+    extra = 1 # How many extra blank rows to show
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('id', 'customer', 'created_at')
+    list_filter = ('created_at',)
+    inlines = [OrderItemInline]
+
+# We don't need to register Customer or OrderItem separately now
+# as they are handled by other admin views.
+# We can register Customer if we want a separate view for it.
 admin.site.register(Customer)
-admin.site.register(Category)
-admin.site.register(Product)
-admin.site.register(Order)
-admin.site.register(OrderItem)
