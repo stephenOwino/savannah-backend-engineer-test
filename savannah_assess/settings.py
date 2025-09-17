3c7383debe254561c11a959691c6ed46fc8fa3db
stephenOwino <36981339s@gmail.com>
2025-09-17 07:33:56 +0300

    switch database configuration from SQLite to PostgreSQL in settings.py

--- savannah_assess/settings.py
+++ savannah_assess/settings.py
@@ -76,8 +76,17 @@ WSGI_APPLICATION = 'savannah_assess.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': '',
+
+        'USER': '',
+
+        'PASSWORD': '',
+
+        'HOST': '',
+
+        'PORT': '',
+        
     }
 }
 
