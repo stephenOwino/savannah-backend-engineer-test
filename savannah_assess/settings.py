208136ff953eba690b6c0838d1c7d840c7e2c1be
stephenOwino <36981339s@gmail.com>
2025-09-17 09:14:10 +0300

    configure PostgreSQL database with environment variables

--- savannah_assess/settings.py
+++ savannah_assess/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -76,16 +77,13 @@ WSGI_APPLICATION = 'savannah_assess.wsgi.application'
 
 DATABASES = {
     'default': {
+        
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'savannadb',
-
-        'USER': 'postgres',
-
-        'PASSWORD': 'Steve@123',
-
-        'HOST': 'localhost',
-
-        'PORT': '5432',
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD'),
+        'HOST': config('DB_HOST'),
+        'PORT': config('DB_PORT', default='5432'),
         
     }
 }
