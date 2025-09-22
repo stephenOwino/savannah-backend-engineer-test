a7fc76b5f4347d2f6ba9699a71d1c4ba1c82dc94
stephenOwino <36981339s@gmail.com>
2025-09-22 18:22:42 +0300

    Update OrderSerializer for testing: remove read_only_fields

--- api/serializers.py
+++ api/serializers.py
@@ -1,5 +1,4 @@
 from rest_framework import serializers
-
 from .models import Category, Order, OrderItem, Product
 
 
@@ -15,12 +14,11 @@ class ProductSerializer(serializers.ModelSerializer):
         fields = ["id", "name", "description", "price", "category", "stock"]
 
 
-# This serializer is for creating an order
 class OrderItemSerializer(serializers.ModelSerializer):
     class Meta:
         model = OrderItem
         fields = ["product", "quantity"]
-
+        
 
 class OrderSerializer(serializers.ModelSerializer):
     products = OrderItemSerializer(many=True)
@@ -28,22 +26,15 @@ class OrderSerializer(serializers.ModelSerializer):
     class Meta:
         model = Order
         fields = ["id", "customer", "created_at", "products", "total_amount"]
-  
+        # Removed read_only_fields for testing
+
     def create(self, validated_data):
-        # Get the product data from the request
         products_data = validated_data.pop("products")
-
-        # Create the main order object
         order = Order.objects.create(**validated_data)
-
-        # Create an OrderItem for each product in the request
         total = 0
         for product_data in products_data:
             order_item = OrderItem.objects.create(order=order, **product_data)
             total += order_item.product.price * order_item.quantity
-
-        # Save the calculated total price
         order.total_amount = total
         order.save()
-
         return order
